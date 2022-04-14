186af10d0307af976c3101929dc09caf59d9ee6a
Morgan Busch <buschmd@clarkson.edu>
2022-04-14 17:29:55 -0400

    Tables and key function

--- des.py
+++ des.py
@@ -1,20 +1,52 @@
 import sys
 from math import ceil
-from typing import List
+from typing import Dict, List
 from des_tables import *
 
 
 # Large help from https://page.math.tu-berlin.de/~kant/teaching/hess/krypto-ws2006/des.htm
 
-def permute(data: int, table:List[int]) -> int:
-    out = [0 for i in range(64)]
-    bits = bin(data).strip('0b')
-    while(len(bits) < 64):
+def padBits(bits, l=64):
+    while(len(bits) < l):
        bits = "0" + bits
+    return bits
+
+def permute(data: int, table:List[int]) -> int:
+    out = []
+    bits = bin(data).strip('0b')
+    bits = padBits(bits)
     for i in range(len(table)):
-        out[table[i] - 1] = bits[i]
+        out.append(bits[table[i]-1])
     outBits = "".join(out)
     return(int(outBits, 2))
+
+def shiftKeys(upper, lower, n):
+    for i in range(n):
+        upper = upper[1:] + upper[0]
+        lower = lower[1:] + lower[0]
+    return upper, lower
+
+
+def getSubkeys(key: int) -> Dict:
+    keys = {"C":[], "D":[] }
+    key = permute(key, PC1)
+    key = bin(key).strip('0b')
+    key = padBits(key, 56)
+    print(f"{key=}")
+
+    upper = key[0:28]
+    lower = key[28:]
+
+    print(f"{upper=} {lower=}")
+    for i in range(16):
+        keys["C"].append(upper)
+        keys["D"].append(lower)
+        upper, lower = shiftKeys(upper, lower, SHIFT[i])
+
+    return keys
+        
+
+
 
 
 
@@ -28,19 +60,28 @@ def message_to_blocks(message: str) -> List[int]:
         chunk = message[chunkSize * i: chunkSize * i+chunkSize]
         block = int.from_bytes(chunk, byteorder='big')
         blocks.append(block)
+        # blocks.append(1383827165325090801)
     
     return blocks
     
 
 def encrypt(message: str, key:str):
     blocks = message_to_blocks(message)
-    p = permute(blocks[0], IP)
-    print(bin(blocks[0]))
-    print(bin(p))
-    pInv = permute(p, IP_INV)
-    print(bin(pInv))
+    # p = permute(blocks[0], IP)
+    # print(bin(blocks[0]))
+    key = 1383827165325090801
+    getSubkeys(key)
+    # print(bin(p))
+    # pInv = permute(p, IP_INV)
+    # print(bin(pInv))
 
 
+def test():
+    print(padBits(bin(1383827165325090801).strip('0b')))
+    keys = getSubkeys(1383827165325090801)
+    print(keys["C"])
+    print(keys["D"])
 
 if __name__ == "__main__":
-    encrypt("AAAAAAAA", "A")
+    test()
+    # encrypt("AAAAAAAA", "A")
